a2094e07b8d848058117eb8489e6a965c8f7e495
shahin1290 <s.patowary@yahoo.com>
2018-11-27 00:14:00 +0100

    Adds winning score argument so that player can choose the round

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,30 +1,33 @@
 class RockPaperScissors
   attr_accessor :player_score
-
-
+  
       def initialize(guess)
-        @plaer_1_guess = guess
+        @player_1_guess = guess
         @player_score = @computer_score = @ties = 0
       end
 
-    def play
-      @computer_choice = random_choice
-  
-      if @plaer_1_guess == @computer_choice
-        return "tie!"
-      elsif (@plaer_1_guess == "rock" && @computer_choice == "scissors") ||
-            (@plaer_1_guess == "paper" && @computer_choice == "rock") ||
-            (@plaer_1_guess == "scissors" && @computer_choice == "paper")
-            @player_score += 1
-        return "won!"
-      else
-        return "lost!"
+    def play(winning_score)
+      while @player_score < winning_score && @computer_score < winning_score         
+       
+        @computer_choice = random_choice
+    
+        if @player_1_guess == @computer_choice
+          return "tie!"
+        elsif (@player_1_guess == "rock" && @computer_choice == "scissors") ||
+              (@player_1_guess == "paper" && @computer_choice == "rock") ||
+              (@player_1_guess == "scissors" && @computer_choice == "paper")
+              @player_score += 1
+          return "won!"
+        else
+          @player_score += 1
+          return "lost!"
+        end
       end
     end
-  
-    def random_choice
-      number_to_choice(Random.new.rand(1...4))
-    end
+    
+      def random_choice
+        number_to_choice(Random.new.rand(1...4))
+      end
 
     private
   
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -5,33 +5,42 @@ describe "Rock Paper Scissors" do
     it "declares tie in case rock vs rock" do
       allow_any_instance_of(RockPaperScissors).to receive(:random_choice).and_return('rock')
       game = RockPaperScissors.new('rock')
-      expect(game.play).to eq "tie!"
+      expect(game.play(10)).to eq "tie!"
     end
   
     it "declares win in case rock beat scissors" do
       allow_any_instance_of(RockPaperScissors).to receive(:random_choice).and_return('scissors')
       game = RockPaperScissors.new('rock')
-      expect(game.play).to eq "won!"
+      expect(game.play(10)).to eq "won!"
     end
   
     it "declares win in case paper beat rock" do
       allow_any_instance_of(RockPaperScissors).to receive(:random_choice).and_return('rock')
       game = RockPaperScissors.new('paper')
-      expect(game.play).to eq "won!"
+      expect(game.play(10)).to eq "won!"
     end
   
     it "declares win in case scissors beat paper" do
       allow_any_instance_of(RockPaperScissors).to receive(:random_choice).and_return('paper')
       game = RockPaperScissors.new('scissors')
-      expect(game.play).to eq "won!"
+      expect(game.play(10)).to eq "won!"
     end
 
-    it "declare final result when anyone score 10" do
+    it "counts players' score" do
       allow_any_instance_of(RockPaperScissors).to receive(:random_choice).and_return('paper')
       game = RockPaperScissors.new('scissors')
-      game.play
-      game.play
+      game.play(10)
+      game.play(10)
       expect(game.player_score).to eq 2
+    end
+
+    it 'playes until winning score is met' do
+      allow_any_instance_of(RockPaperScissors).to receive(:random_choice).and_return('paper')
+      game = RockPaperScissors.new('scissors')
+      12.times {
+        game.play(10)
+      }
+      expect(game.player_score).to eq 10
     end
   
 end
